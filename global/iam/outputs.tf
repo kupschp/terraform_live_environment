25342a135544cd4e4f4d6ba1ce006af672aa4651
Philip Kupsch <pkupsch@wayfair.com>
2023-01-10 16:29:19 +0100

    example of using if in terraform

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -6,3 +6,7 @@ output "iam_all_users" {
 output "upper_user_name" {
   value = [for user_name in var.user_names : upper(user_name)]
 }
+
+output "short_upper_names" {
+  value = [for user_name in var.user_names : upper(user_name) if length(user_name) < 5]
+}
