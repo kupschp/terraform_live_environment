b233504cbc5f80fc7a1a2b88ff1d32d25f646542
Philip Kupsch <pkupsch@wayfair.com>
2023-01-10 16:27:15 +0100

    example of using for loop to change variabel value in terraform

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -2,3 +2,7 @@ output "iam_all_users" {
   value = aws_iam_user.ptg-iam-users
   description = "second iam user created"
 }
+
+output "upper_user_name" {
+  value = [for user_name in var.user_names : upper(user_name)]
+}
